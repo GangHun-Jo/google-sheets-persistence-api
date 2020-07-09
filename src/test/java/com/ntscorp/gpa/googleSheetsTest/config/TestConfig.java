31aae76c8a7a844285f0a5ea5820a2d8d55f206e
ganghun.cho <ganghun.cho@nts-corp.com>
2020-07-09 15:31:59 +0900

    add test case - add leftjoin test case

---
+++ src/test/java/com/ntscorp/gpa/googleSheetsTest/config/TestConfig.java
@@ -0,0 +1,131 @@
+package com.ntscorp.gpa.googleSheetsTest.config;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.stereotype.Repository;
+
+import com.ntscorp.gpa.googleSheets.GoogleSheetsRepository;
+import com.ntscorp.gpa.googleSheets.LeftJoin;
+
+
+@TestConfiguration
+public class TestConfig {
+
+	public static class Employee {
+		private int id;
+		private String name;
+		private int age;
+		private LocalDateTime birthday;
+		@LeftJoin(joinColumn = "employeeId", targetClass = Asset.class)
+		private List<Asset> assetList;
+
+		public Employee() {
+			super();
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getAge() {
+			return age;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
+		}
+
+		public LocalDateTime getBirthday() {
+			return birthday;
+		}
+
+		public void setBirthday(LocalDateTime birthday) {
+			this.birthday = birthday;
+		}
+
+		public List<Asset> getAssetList() {
+			return assetList;
+		}
+
+		public void setAssetList(List<Asset> assetList) {
+			this.assetList = assetList;
+		}
+
+		@Override
+		public String toString() {
+			return String.join(" ", Integer.toString(id), name, Integer.toString(age), birthday.toString(), assetList.toString());
+		}
+	}
+
+	public static class Asset {
+		private int id;
+		private int employeeId;
+		private String name;
+		private LocalDateTime purchaseDateTime;
+
+		public Asset() {
+			super();
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public int getEmployeeId() {
+			return employeeId;
+		}
+
+		public void setEmployeeId(int employeeId) {
+			this.employeeId = employeeId;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public LocalDateTime getPurchaseDateTime() {
+			return purchaseDateTime;
+		}
+
+		public void setPurchaseDateTime(LocalDateTime purchaseDateTime) {
+			this.purchaseDateTime = purchaseDateTime;
+		}
+
+		@Override
+		public String toString() {
+			return String.join(" ", Integer.toString(id), name, purchaseDateTime.toString());
+		}
+	}
+
+	// left join시 bean 의 이름으로 검색하므로 이름을 설정해주어야함
+	// TODO : bean의 이름 대신 type으로 검색하는 방식으로 구현해보
+	@Repository(value = "assetRepository")
+	public static class AssetRepository extends GoogleSheetsRepository<Asset> {
+	}
+
+	@Repository(value = "employeeRepository")
+	public static class EmployeeRepository extends GoogleSheetsRepository<Employee> {
+	}
+}
--- src/test/java/com/ntscorp/gpa/googleSheetsTest/googleSheetsRepositoryTest.java
+++ src/test/java/com/ntscorp/gpa/googleSheetsTest/googleSheetsRepositoryTest.java
@@ -1,74 +1,23 @@
 package com.ntscorp.gpa.googleSheetsTest;
 
-import java.time.LocalDateTime;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.stereotype.Repository;
+import org.springframework.context.annotation.Import;
 
-import com.ntscorp.gpa.googleSheets.GoogleSheetsRepository;
+import com.ntscorp.gpa.googleSheetsTest.config.TestConfig;
+import com.ntscorp.gpa.googleSheetsTest.config.TestConfig.EmployeeRepository;
 
 @SpringBootTest
+@Import(TestConfig.class)
 public class googleSheetsRepositoryTest {
 
-	public static class Employee {
-		int id;
-		String name;
-		int age;
-		LocalDateTime birthday;
-
-		public Employee() {
-			super();
-		}
-
-		public int getId() {
-			return id;
-		}
-
-		public void setId(int id) {
-			this.id = id;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public int getAge() {
-			return age;
-		}
-
-		public void setAge(int age) {
-			this.age = age;
-		}
-
-		public LocalDateTime getBirthday() {
-			return birthday;
-		}
-
-		public void setBirthday(LocalDateTime birthday) {
-			this.birthday = birthday;
-		}
-	}
-
-
-	@TestConfiguration
-	static class TestConfig {
-		@Repository
-		static class EmployeeRepository extends GoogleSheetsRepository<Employee> {
-		}
-	}
-
 	@Autowired
-	private TestConfig.EmployeeRepository employeeRepository;
+	private EmployeeRepository employeeRepository;
 
 	@Test
 	public void ex() {
+
 		System.out.println(employeeRepository.getAll());
 	}
 }
